5bf690147cadda9d03c50cd37233045fbf8155e1
Mehul Parmar <mehulparmar5028@gmail.com>
2023-11-26 23:12:16 +0530

    login data validation

--- src/Components/UserModelProvider.js
+++ src/Components/UserModelProvider.js
@@ -22,6 +22,8 @@ export default function UserModelProvider(props) {
                     return res.json();
                 }
                 else {
+                    const msg = document.getElementById('wrongInputMsg');
+                    msg.style.display = "block";
                     console.log("Some errror occured!!");
                     throw new Error("login faild");
                 }
@@ -80,16 +82,88 @@ export default function UserModelProvider(props) {
                         <>
                             <div className='login' id='login'>
                                 <h2>Log in</h2>
-                                <input type="text" className='ip' style={{ marginBottom: "3vh" }} placeholder="email" onChange={(e) => {
-                                    e.preventDefault();
-                                    setUser({ ...user, email: e.target.value });
-                                }} />
-                                <i class="fa-solid fa-envelope" style={{ color: "white", marginLeft: "-2vh" }}></i><br />
-                                <input type="password" className='ip' style={{ marginBottom: "5vh" }} placeholder="password" onChange={(e) => {
-                                    e.preventDefault();
+                                <p id="wrongInputMsg">Invalid email or password</p>
+                                <i class="fa-solid fa-envelope" style={{ color: "white" }} ></i>
+                                <input type="text" id="email" className='ip' style={{ marginBottom: "3vh" }} placeholder="email" onChange={(e) => {
+                                    const pattern = "^[A-Za-z]+[0-9]+@+gmail+[\.]+com"
+                                    const emailvalue = document.getElementById('email').value;
+                                    const icon = document.getElementById('icon');
+                                    const regEx = new RegExp(pattern);
+                                    const msg = document.getElementById('wrongInputMsg');
+                                    msg.style.display = "none";
+                                    if (!regEx.test(emailvalue)) {
+                                        icon.style.color = "red";
+                                    } else {
+                                        icon.style.color = "green";
+                                        setUser({ ...user, email: e.target.value });
+                                    }
+                                    if (e.target.value === "") {
+                                        icon.style.color = "white";
+                                    }
+                                }} /><i class="fa-regular fa-circle-check" id="icon"></i><br />
+
+                                <i class="fa-solid fa-lock" style={{ color: "white" }}></i>
+                                <input type="password" id="password" className='ip' style={{ marginBottom: "5vh" }} placeholder="password" onChange={(e) => {
+                                    const msg = document.getElementById('wrongInputMsg');
+                                    msg.style.display = "none";
+                                    const pass = document.getElementById("password").value;
+                                    const icon = document.getElementById('icon-right');
+                                    const capitalLetter = "[A-Z]";
+                                    const smallLetter = "[a-z]";
+                                    const specialLettre = "[~!@#$%^&*()_+=-{}]";
+                                    const number = "[0-9]";
+                                    const regExCap = new RegExp(capitalLetter);
+                                    const regexSmall = new RegExp(smallLetter);
+                                    const regExSpLett = new RegExp(specialLettre);
+                                    const regExNum = new RegExp(number);
+                                    const msg_list = document.getElementById('list-msg');
+                                    // if(pass.length() >= 8){
+                                    if (!regExCap.test(pass) || !regexSmall.test(pass) || !regExSpLett.test(pass) || !regExNum.test(pass)) {
+                                        icon.style.color = "red";
+                                        msg_list.style.display = "block";
+                                    }
+                                    else {
+                                        icon.style.color = "green";
+                                    }
+                                    if (e.target.value === "") {
+                                        msg_list.style.display = "none";
+                                        icon.style.color = "white";
+                                    }
+                                    if (regExCap.test(pass) && regexSmall.test(pass) && regExSpLett.test(pass) && regExNum.test(pass)) {
+                                        msg_list.style.display = "none";
+                                    }
+                                    // }
                                     setUser({ ...user, password: e.target.value });
-                                }} /><i class="fa-solid fa-lock" style={{ color: "white", marginLeft: "-2vh" }}></i><br />
-                                <button type="submit" className="btn btn-primary" onClick={sendModel} style={{ width: "70%", borderRadius: "50px", marginBottom: "2vh" }}>Log in</button>
+                                }} />
+                                <i class="fa-solid fa-eye" style={{ color: "white" }} id="eye-icon" onClick={()=>{
+                                    const pass = document.getElementById("password");
+                                    const eye_open = document.getElementById('eye-icon');
+                                    const btn = document.getElementById('eye-close');
+                                    btn.classList.toggle('active');
+                                    if(btn.classList.contains('active')){
+                                        eye_open.style.display = "none";
+                                        pass.type = "text";
+                                    }
+                                }}></i>
+                                <i class="fa-solid fa-eye-slash" id="eye-close" onClick={()=>{
+                                    const pass = document.getElementById("password");
+                                    const btn = document.getElementById('eye-close');
+                                    const eye_open = document.getElementById('eye-icon');
+                                    btn.classList.toggle('active');
+                                    if(!btn.classList.contains('active')){
+                                        eye_open.style.display = "inline";
+                                        pass.type = "password";
+                                    }
+                                }}></i>
+                                <i id='icon-right' class="fa-regular fa-circle-check" style={{ color: "white" }}></i>
+                                <ul id="list-msg">
+                                    <li>Password must contain atleast 1 capital letter</li>
+                                    <li>Password must contain atleast 1 small letter</li>
+                                    <li>Password must contain atleast 1 number</li>
+                                    <li>Password must contain atleast 1 special charcter</li>
+                                </ul>
+
+                                <button type="submit" className="btn btn-primary" style={{ width: "70%", borderRadius: "50px", marginBottom: "2vh" }} onClick={sendModel}>Log in</button>
                                 <p style={{ color: "white" }}>Don't have an account?<Link to={'/food/auth/signup'}>Sign up</Link></p>
                             </div>
 
